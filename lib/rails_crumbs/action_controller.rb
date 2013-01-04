f30c89dad39130e5235097f4d586c0fafd53027b
Mathías Montossi <mmontossi@mattways.com>
2013-01-04 10:07:40 -0200

    Code climate, travis ci and gemnasium

--- lib/rails_crumbs/action_controller.rb
+++ lib/rails_crumbs/action_controller.rb
@@ -20,39 +20,22 @@ module RailsCrumbs
       module InstanceMethods
       
         def self.included(base)
-          base.send :before_filter, :crumbs
+          base.send :before_filter, :define_crumbs
         end
 
         protected    
   
-        def crumbs    
-     
-          if session[:referer].nil?
-            session[:referer] = [{:base_url => request.base_url, :path => request.path, :fullpath => request.fullpath}]
-          elsif is_last_referer?
-            last_index = session[:referer].size - 1
-            session[:referer][last_index][:fullpath] = request.fullpath
-          elsif add_to_referer?
-            session[:referer] << {:base_url => request.base_url, :path => request.path, :fullpath => request.fullpath}
-          elsif index = in_referer_tree?
-            session[:referer] = session[:referer].slice(Range.new(0, index))    
-            session[:referer] << {:base_url => request.base_url, :path => request.path, :fullpath => request.fullpath}
-          else
-            session[:referer] = [{:base_url => request.base_url, :path => request.path, :fullpath => request.fullpath}]            
-          end       
-        
+        def define_crumbs    
+          
+          check_referer
+    
           parts = request.path.split('/')
           parts.pop 
           path = join_parts(parts)           
         
           @crumbs = []    
           while parts.size > 0   
-            begin
-              params = Rails.application.routes.recognize_path(request.base_url + path) 
-            rescue ::ActionController::RoutingError => e
-              params = false
-            end
-            if params
+            if params = find_path_params(path)
               if name = Crumbs.get_name(params[:controller], params[:action], params)
                 if index = in_referer?(path)
                   path = session[:referer][index][:fullpath]
@@ -63,8 +46,36 @@ module RailsCrumbs
             parts.pop      
             path = join_parts(parts)
           end  
-          @crumbs.reverse!    
+          @crumbs.reverse!  
     
+        end
+
+        def find_path_params(path)
+          begin
+            Rails.application.routes.recognize_path(request.base_url + path) 
+          rescue ::ActionController::RoutingError => e
+            false
+          end
+        end
+
+        def check_referer
+          if session[:referer].nil?
+            session[:referer] = [request_referer]
+          elsif is_last_referer?
+            last_index = session[:referer].size - 1
+            session[:referer][last_index][:fullpath] = request.fullpath
+          elsif add_to_referer?
+            session[:referer] << request_referer 
+          elsif index = in_referer_tree?
+            session[:referer] = session[:referer].slice(Range.new(0, index))    
+            session[:referer] << request_referer
+          else
+            session[:referer] = [request_referer]            
+          end  
+        end
+
+        def request_referer
+          {:base_url => request.base_url, :path => request.path, :fullpath => request.fullpath}
         end
         
         def join_parts(parts)
@@ -83,25 +94,32 @@ module RailsCrumbs
           parts.size > 0 and last[:path] == join_parts(parts.slice(0, parts.size - 1))
         end
 
+        def in_referer?(path)
+          find_in_referer path
+        end
+
         def in_referer_tree?
           parts = request.path.split('/')
           parts.pop
           while parts.size > 0
-            index = session[:referer].index do |referer|
-              referer[:base_url] == request.base_url and 
-              referer[:path] == join_parts(parts) and
-              referer[:path] != request.path
-            end
-            return index unless index.nil?
+            return index = find_in_referer_tree(parts) unless index.nil?
             parts.pop
-          end        
+          end
         end
 
-        def in_referer?(path)
-          index = session[:referer].index do |referer|
+        def find_in_referer(path)
+          session[:referer].index do |referer|
             referer[:base_url] == request.base_url and referer[:path] == path
           end
-        end 
+        end         
+
+        def find_in_referer_tree(parts)
+          session[:referer].index do |referer|
+            referer[:base_url] == request.base_url and 
+            referer[:path] == join_parts(parts) and
+            referer[:path] != request.path
+          end
+        end
        
       end  
     end
--- lib/rails_crumbs/crumbs.rb
+++ lib/rails_crumbs/crumbs.rb
@@ -7,7 +7,7 @@ module RailsCrumbs
       end
     
       def add(controller, action, name)
-        if all[controller.to_sym].nil?
+        unless all.has_key? controller.to_sym
           all[controller.to_sym] = {action.to_sym => name}
         else 
           all[controller.to_sym][action.to_sym] = name
@@ -15,7 +15,7 @@ module RailsCrumbs
       end
   
       def get_name(controller, action, params)
-        return false if all[controller.to_sym].nil?
+        return false unless all.has_key? controller.to_sym
         name = all[controller.to_sym][action.to_sym]
         case name
         when String
