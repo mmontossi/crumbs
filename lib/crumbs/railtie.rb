acf172188faf7d227b3a4fc474df3a9d2ea2e610
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 14:50:36 -0300

    Minor improvements

--- lib/crumbs/railtie.rb
+++ lib/crumbs/railtie.rb
@@ -1,14 +1,16 @@
 module Crumbs
   class Railtie < Rails::Railtie
 
-    initializer 'crumbs.extensions' do
-      ::ActionController::Base.include(
-        Crumbs::Extensions::ActionController::Base
-      )
+    config.before_initialize do
+      load Rails.root.join('config/crumbs.rb')
     end
 
-    config.after_initialize do
-      load Rails.root.join('config/crumbs.rb')
+    initializer 'crumbs.action_controller' do
+      ActiveSupport.on_load :action_controller do
+        ::ActionController::Base.include(
+          Crumbs::Extensions::ActionController::Base
+        )
+      end
     end
 
   end
